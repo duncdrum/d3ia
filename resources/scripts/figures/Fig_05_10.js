e408f0a484c4f40cf266b498bd5de755245e3053
Duncan Paterson <d.paterson@me.com>
2016-08-31 17:03:51 +0200

    !so to the rescue, i give up

--- resources/scripts/figures/Fig_05_10.js
+++ resources/scripts/figures/Fig_05_10.js
@@ -1,11 +1,53 @@
-d3.json("../data/source/tweets.json", function (error, data) {
-    dataViz(data.tweets)
+d3.json("../data/source/tweets.json", function(error,data) {
+dataViz(data.tweets)
 });
 
-function dataViz(incData) {
+    function dataViz(incData) {
+
+      nestedTweets = d3.nest()
+      .key(function (el) {return el.user})      
+      .entries(incData);
+      
+     /* packableTweets = d3.hierarchy({id: "root", values: nestedTweets}, function children(d) {return d.children ;})
+	  .sum(function(d) { return 1;  });*/
+	  
+    packableTweets = d3.hierarchy({id: "root", values: nestedTweets}, function children(d) {return d.values})
+    .eachBefore(function(d) { return 1;  })
+    .sum(function(d) {d.values ? 1 : 0});
     
-    /*Maniputlate incoming JSON to match d3.pack function*/
-    /*TODO this kind of manipulation should move out of JS into xQuery*/
+    
+
+      var depthScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+	 exposedData = incData;
+      packChart = d3.pack()
+	  .size([500,500])
+	  .padding(3);
+
+     
+      
+      
+      d3.select("svg")
+      .append("g")
+      .attr("transform", "translate(0,0)")
+      .selectAll("circle")
+      .data(packChart(packableTweets))
+      .enter()
+      .append("circle")
+      .attr("r", function(d) {return d.r - (d.depth * 0)})
+      .attr("cx", function(d) {return d.x})
+      .attr("cy", function(d) {return d.y})
+      
+      
+      
+      .style("fill", function(d) {return depthScale(d.depth)})
+      .style("stroke", "black")
+      .style("stroke", "2px")
+    }
+/*function dataViz(incData) {
+    
+    /\*Maniputlate incoming JSON to match d3.pack function*\/
+    /\*TODO this kind of manipulation should move out of JS into xQuery*\/
     
     nestedTweets = d3.nest()
     .key(function (el) {
@@ -14,14 +56,11 @@ function dataViz(incData) {
     .entries(incData);
     
     packableTweets = d3.hierarchy({id: "root", values: nestedTweets});
-    /*root(
-    .node.children (function(d) {return d.values})
-    .node.value (function(d) {return 1}));*/
-    
-    
+    packableTweets
+    .sum(function(d) { return d.values ? 0 : 1 })
+    .sort(function(a, b) { return b.height - a.height || b.value - a.value; })
+    .descendants();
 
-
-/*root.sum(function(d) { return d.value ? 1 : 0; });*/
     
     var depthScale = d3.scaleOrdinal(d3.schemeCategory10);
     
@@ -31,16 +70,18 @@ function dataViz(incData) {
     .padding(1)
     ;   
     
-    var nodes = packableTweets(root
+    packChart(packableTweets);*/
+    
+/*    var nodes = packableTweets(root
     .sum (function(d) { return d.values; })
-    .value (function(d) {return 1}));
+    .value (function(d) {return 1}));*/
 /*    .sort (function(a, b) { return b.height - a.height || b.values - a.values; }))*/
     
     
 /*    var nodes = treemap(root
     .sum(function(d) { return d.value; })
     .sort(function(a, b) { return b.height - a.height || b.value - a.value; }))
-  .descendants();*/
+  .descendants();
   
     
     
@@ -60,23 +101,5 @@ function dataViz(incData) {
       .style("stroke", "black")
       .style("stroke", "2px")
       
-/*      .style("fill", function(d) { return color(d.depth); })*/
-    
-  /*  d3.select("svg")
-    .append("g")
-    .attr("transform", "translate(0,0)")
-    /\*    .data(packChart(packableTweets))*\/
-    .data(packableTweets.descendants())
-    .enter()
-    .selectAll("circle")
-    .append("circle")
-    .attr("cx", function (d) {
-        return d.x
-    })
-    .attr("cy", function (d) {
-        return d.y
-    })
-    .style("fill", function (d) {
-        return depthScale(d.depth)
-    })*/
-}
+
+}*/
