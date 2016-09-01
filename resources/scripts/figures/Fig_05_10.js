5eb25ccc1fdabf150cc562c6952297fc9c04c43a
Duncan Paterson <d.paterson@me.com>
2016-09-01 13:40:55 +0200

    ch5 progress at last

--- resources/scripts/figures/Fig_05_10.js
+++ resources/scripts/figures/Fig_05_10.js
@@ -6,40 +6,35 @@ dataViz(data.tweets)
 
       nestedTweets = d3.nest()
       .key(function (el) {return el.user})      
-      .entries(incData);
-      
-     /* packableTweets = d3.hierarchy({id: "root", values: nestedTweets}, function children(d) {return d.children ;})
-	  .sum(function(d) { return 1;  });*/
-	  
-    packableTweets = d3.hierarchy({id: "root", values: nestedTweets}, function children(d) {return d.values})
-    .eachBefore(function(d) { return 1;  })
-    .sum(function(d) {d.values ? 1 : 0});
+      .entries(incData);    
     
-    
+    packableTweets = d3.hierarchy({id: "root", values: nestedTweets.entries(incData)},
+    function (d) {
+        return d.values
+    })
+    .sum(function(d) { return d.children ? 0 : 1; })
+    .sort(null);
 
       var depthScale = d3.scaleOrdinal(d3.schemeCategory10);
 
 	 exposedData = incData;
       packChart = d3.pack()
 	  .size([500,500])
-	  .padding(3);
-
-     
+	  .padding(3);    
       
+      packChart(packableTweets);
       
       d3.select("svg")
       .append("g")
-      .attr("transform", "translate(0,0)")
+        .attr("transform", "translate(0,0)")        
       .selectAll("circle")
-      .data(packChart(packableTweets))
+      .data(packableTweets.descendants())
       .enter()
       .append("circle")
-      .attr("r", function(d) {return d.r - (d.depth * 0)})
+      .attr("r", function(d) {return d.r - (d.depth * 0)})          
       .attr("cx", function(d) {return d.x})
       .attr("cy", function(d) {return d.y})
-      
-      
-      
+      /*.merge(circles) */       
       .style("fill", function(d) {return depthScale(d.depth)})
       .style("stroke", "black")
       .style("stroke", "2px")
