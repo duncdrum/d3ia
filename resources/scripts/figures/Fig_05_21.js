dbc433a33c632add5567a6ae071d64d9ee7c5fe8
Duncan Paterson <d.paterson@me.com>
2016-09-03 18:07:40 +0200

    nuclear option…
    not as nukey as I d like.

--- resources/scripts/figures/Fig_05_21.js
+++ resources/scripts/figures/Fig_05_21.js
@@ -1,10 +1,11 @@
 d3.csv("../data/source/movies.csv", function (error, data) {
+    delete data.columns; /*nuclear option*/ 
     dataViz(data)
 });
 
-function dataViz(incData) {
-    expData = incData;
-    stackData =[];
+function dataViz(incData) {    
+/*    expData = incData;*/
+    stackData =[];    
     
     var xScale = d3.scaleLinear()
     .domain([0, 10])
@@ -19,17 +20,18 @@ function dataViz(incData) {
     var stackArea = d3.area()
     .curve(d3.curveBasis)
     .x(function(d) { return xScale(d.x); })
-    .y0(500)
-    .y1(function(d) { return yScale(d.y); });
+    .y0(function(d) { return yScale(0); })
+    .y1(function(d) { return yScale(0 + d.y); });
+    
     
     
     for (x in incData[0]) {
         if (x != "day") {
             var newMovieObject = {
                 name: x, values:[]
-            };             
-            for (y in incData) {
-                newMovieObject
+            };
+            for (y in incData) { //incData.slice(0, 10)
+                newMovieObject                
                 .values
                  .push({
                     x: parseInt(incData[y][ "day"]), 
@@ -61,10 +63,10 @@ function dataViz(incData) {
     
     
     stackLayout = d3.stack()
-    .keys(["movie1","movie2","movie3","movie4","movie5","movie6"])
+/*    .keys(incData.columns)*/
     .offset(d3.stackOffsetSilhouette)
     .order(d3.stackOrderInsideOut)
-/*    .value(function (d) { return d.values});*/
+    .value(function (d) { return d.values});
     
     stackLayout(stackData);
     
@@ -73,10 +75,11 @@ function dataViz(incData) {
     .selectAll("path")
     .data(stackData)
     .enter()
-/*    .merge(stackData.values)*/
+
     .append("path")
     .style("fill", 
         function (d) { return movieColors(d.name); })
     .attr("d",
-        function (d) { return stackArea(d.values);})
+        function(d) { return stackArea(d.values); })
+/*    .merge(stackData)*/
 }
--- temp/Untitled4.js
+++ temp/Untitled4.js
@@ -1,5 +1,51 @@
-d3.csv("../data/source/movies.csv", function (error, data) {
-    if (error) throw error;
-  console.log(data); 
-});
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+
+var x = d3.scaleLinear()
+    .domain([0, 10])
+    .range([0, 500]);
+    
+var y = d3.scaleLinear()
+    .domain([0, 100])
+    .range([500, 0]);   
+    
+var z = d3.scaleOrdinal(d3.schemeCategory10);    
+    
+var stack = d3.stack();    
+    
+    
+d3.csv("../data/source/movies.csv", function (error, data) {
+/*    delete data.columns;  */
+    if (error) throw error;
+    
+
+  x.domain(data.map(function(d) { return d.x; }));
+  y.domain([0, d3.max(data, function(d) { return d.y; })]).nice();
+  z.domain(data.columns.slice(1));
+  
+  g.selectAll(".serie")
+    .data(stack.keys(data.columns.slice(1))(data))
+    .enter().append("g")
+      .attr("class", "serie")
+      .attr("fill", function(d) { return z(d.key); })
+    .selectAll("rect")
+    .data(function(d) { return d; })
+    .enter().append("rect")
+      .attr("x", function(d) { return x(d.data.x); })
+      .attr("y", function(d) { return y(d[1]); })
+      .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+      .attr("width", x.bandwidth());
+ });   
+    
+   
+ function type(d, i, columns) {
+  for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+  d.total = t;
+  return d;
+}   
+    
+   
