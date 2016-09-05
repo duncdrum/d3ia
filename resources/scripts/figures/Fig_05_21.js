0084382fcf5cae4c5eb9503c1d7a0dff28cf175e
Duncan Paterson <d.paterson@me.com>
2016-09-05 17:21:41 +0200

    hale bob good enough

--- resources/scripts/figures/Fig_05_21.js
+++ resources/scripts/figures/Fig_05_21.js
@@ -4,20 +4,11 @@ d3.csv("../data/source/movies.csv", function (error, data) {
   });
   dataViz(data)
 });
-/*    delete data.columns; /*nuclear option*/
 
-
-
-/*d3.csv('../data/source/movies.csv', function(error, data) {
-  movieColors.domain(d3.keys(data[0]).filter(function(key) { return key !== 'day'; }));
-  var keys = data.columns.filter(function(key) { return key !== 'day'; })
-  data.forEach(function(d) {
-    d.day = parseInt(d.day); });
-        dataViz(data);});*/
 
 function dataViz(incData) {    
 /*    expData = incData;*/
-    var keys = incData.columns.slice(1); //"movie1", "movie2", etc. not "day"
+    var keys = incData.columns.slice(1); //"movie1", "movie2", etc. Not "day"
     stackData = incData; 
     
     var xScale = d3.scaleLinear()
@@ -31,7 +22,7 @@ function dataViz(incData) {
     var movieColors = d3.scaleOrdinal(d3.schemeCategory10);
     
     function type(d, i, columns) {
-        d.day = parseInt(d.day);
+/*        d.day = parseInt(d.day);*/
         for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = d[columns[i]] ;
         return d;
     }
@@ -43,54 +34,23 @@ function dataViz(incData) {
     .order(d3.stackOrderInsideOut)
 /*    .value(function(d){return (d)});*/
 
-
-    
-/*    stackLayout(stackData);*/
     
     var stackArea = d3.area()
     .curve(d3.curveBasis)
     .x(function(d, i) { return xScale(d.data.day); })
     .y0(function(d) { return yScale(d[0]); })
     .y1(function(d) { return yScale(d[1]); });
-    
-    
-/*  function type(d, i, columns) {
-  d.day = parseInt(d.day);
-  for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = d[columns[i]] ;
-  return d;
-}*/
-    
-    /*for (x in incData[0]) {
-      if (x != "day") {var newMovieObject = {
-                name: x, values:[]
-            };
-    for (y in incData.slice(0, 10)) { //incData.slice(0, 10) vs incData
-                newMovieObject                
-                .values
-                 .push({
-                    x: parseInt(incData[y][ "day"]), 
-                    y: parseInt(incData[y][x])
-                })
-            }
-            stackData
-            .push(newMovieObject);
-            }
-     }*/
-        
-
-   
-    
-    
-    
+     
     
     d3.select("svg")
     .selectAll("path")
     .data(stackLayout(stackData))
     .enter()
     .append("path")
+/*    .append('g')*/
+    .attr('transform', 'translate(' + -20 + ',' + -150 + ')')
     .style("fill", 
         function (d) { return movieColors(d.key); })
     .attr("d",
-        function(d) { return stackArea; })
-/*    .merge(stackData)*/
+        function(d) { return stackArea(d); })
 }
